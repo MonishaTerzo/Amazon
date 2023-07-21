1e4aee0803b6d0a2ddfd06119a2fb7a4ed6a1c3a
MonishaTerzo <136312524+MonishaTerzo@users.noreply.github.com>
2023-07-21 10:53:46 +0530

    Update CartController.java

--- src/main/java/com/ecommerce/amazon/controller/CartController.java
+++ src/main/java/com/ecommerce/amazon/controller/CartController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.amazon.controller;
 
 import com.ecommerce.amazon.dto.CartDto;
+import com.ecommerce.amazon.dto.CartItemsDto;
+import com.ecommerce.amazon.model.CartItems;
 import com.ecommerce.amazon.model.Category;
 import com.ecommerce.amazon.model.Product;
 import com.ecommerce.amazon.service.CartItemService;
+import com.ecommerce.amazon.service.CartService;
 import com.ecommerce.amazon.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,41 +14,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class CartController {
 
-    private final CartItemService cartItemService;
+    private final CartService cartItemService;
     private final ProductService productService;
 
     @Autowired
-    public CartController(CartItemService cartItemService, ProductService productService) {
+    public CartController(CartService cartItemService, ProductService productService) {
         this.cartItemService = cartItemService;
         this.productService = productService;
     }
 
 
     @PostMapping("AddToCart/{userId}")
-    public ResponseEntity<CartDto> addItemToCart(@RequestBody CartDto cartItemDTO, @PathVariable Long userId) {
-        CartDto addedCartItem = cartItemService.addItemToCart(cartItemDTO, userId);
-        return new ResponseEntity<>(addedCartItem, HttpStatus.CREATED);
+    public boolean addItemToCart(@RequestBody CartItemsDto cartItems, @PathVariable Long userId) {
+        CartDto addedCartItem = cartItemService.addItemToCart(cartItems, userId);
+        return true;
     }
-
 
 
     @GetMapping("/RecommendedProducts/{userId}")
     public List<List<Product>> recommendedProducts(@PathVariable("userId") Long userId) {
-        List<Category> categoryList = cartItemService.getProductFromCart(userId).stream()
-                .map(x->x.getCategory())
-                .collect(Collectors.toList());
-        List<List<Product>> products = categoryList.stream()
-                .map(x->productService.getProductsByCategory(x.getName()))
-                .collect(Collectors.toList());
+//        List<Category> categoryList = cartItemService.getProductFromCart(userId).stream()
+//                .map(x->x.getCategory())
+//                .collect(Collectors.toList());
+//        List<List<Product>> products = categoryList.stream()
+//                .map(x->productService.getProductsByCategory(x.getName()))
+//                .collect(Collectors.toList());
 
-        return products;
+        return null;
     }
 
 }
