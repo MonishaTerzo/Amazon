bf33b813d885b48425f0e620a42da30053d2d525
MonishaTerzo <136312524+MonishaTerzo@users.noreply.github.com>
2023-07-21 10:55:06 +0530

    Update OrderController.java

--- src/main/java/com/ecommerce/amazon/controller/OrderController.java
+++ src/main/java/com/ecommerce/amazon/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.ecommerce.amazon.controller;
 
 import com.ecommerce.amazon.dto.OrderDto;
 import com.ecommerce.amazon.model.Order;
-import com.ecommerce.amazon.model.Product;
 import com.ecommerce.amazon.model.User;
 import com.ecommerce.amazon.service.OrderService;
 import com.ecommerce.amazon.service.UserService;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 @RestController
@@ -23,35 +23,37 @@ public class OrderController {
             this.orderService = orderService;
             this.userService=userService;
         }
+
         @GetMapping("/{userId}/delivered")
         public List<OrderDto> getDeliveredOrdersByUser(@PathVariable int userId) {
             User user = userService.getUserById(userId);
             return mapToDtoList(orderService.getDeliveredOrdersByUser(user));
-
         }
+
     private List<OrderDto> mapToDtoList(List<Order> orders) {
             return orders.stream()
                     .map(x->mapToDto(x))
                     .collect(Collectors.toList());
     }
+
     private OrderDto mapToDto(Order order) {
             return OrderDto.builder()
-                    .orderDate(order.getOrderDate())
                     .expectedDeliveryDate(order.getExpectedDeliveryDate())
                     .isPaid(order.isPaid())
                     .isShipped(order.isShipped())
                     .outForDelivery(order.isOutForDelivery())
                     .shippingAddress(order.getShippingAddress())
-                    .shippingdate(order.getShippingdate())
                     .paymentMethod(order.getPaymentMethod())
                     .totalAmount(order.getTotalAmount())
                     .username(order.getUsername())
                     .build();
     }
-    @PostMapping("/placeOrder")
+
+    @PostMapping("/place-order")
     public ResponseEntity<String> createOrder(@RequestBody OrderDto orderDTO) {
         orderService.createOrder(orderDTO);
         return new ResponseEntity<>("Order created successfully.", HttpStatus.CREATED);
 
     }
+
 }
